0ac3fdeef6c1fbe7da569ce5ff077cfa23d55af5
Ari Shaverin <arishaverin@icloud.com>
2026-03-09 17:50:42 +0000

    swiftping full infrastructure setup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,1 @@
-\"use\ client\"\; 
-import\ {supabase}\ from\ \"../../lib/supabase\"\; 
-export\ default\ function\ Login\(\)\{ 
-const\ login\=\ async\(\)\=\
-const\ email\=document.getElementById\(\"email\"\)\.value\; 
-const\ pass\=document.getElementById\(\"pass\"\)\.value\; 
-await\ supabase\.auth\.signInWithPassword\(\{email\:email\,password\:pass\}\)\; 
-alert\(\"Logged\ in\"\)\}\; 
+\"use\ client\" 
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,8 +1,1 @@
-\"use\ client\"\; 
-import\ {supabase}\ from\ \"../../lib/supabase\"\; 
-export\ default\ function\ Signup\(\)\{ 
-const\ signup\=\ async\(\)\=\
-const\ email\=document.getElementById\(\"email\"\)\.value\; 
-const\ pass\=document.getElementById\(\"pass\"\)\.value\; 
-await\ supabase\.auth\.signUp\(\{email\:email\,password\:pass\}\)\; 
-alert\(\"Account\ created\"\)\}\; 
+\"use\ client\" 
