b68807e9bda09927e61311e2c43d322bf0efd083
Ari Shaverin <arishaverin@icloud.com>
2026-03-09 18:30:21 +0000

    fix css import paths

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Link from "next/link"
-import "./globals.css"
+import "../globals.css"
 
 export default function Home() {
   return (
--- app/support/page.tsx
+++ app/support/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import "./globals.css";
+import "../globals.css";
 import Link from "next/link";
 
 export default function Home() {
