888cc46ef00db746ff8ee2f6a534aebf50c9e373
Ari Shaverin <arishaverin@icloud.com>
2026-03-10 10:24:01 +0000

    restore homepage

---
+++ app/page.tsx
@@ -0,0 +1,4 @@
+import Link from "next/link";
+export default function Home(){
+return (<main style={{background:"#020617",color:"white",minHeight:"100vh",fontFamily:"Arial"}}><h1 style={{fontSize:"60px",padding:"40px"}}>SwiftPing</h1></main>);
+}
