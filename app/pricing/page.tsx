8f6213cd577493fd604c5927b64aab66d9b229f3
Ari Shaverin <arishaverin@icloud.com>
2026-03-10 10:59:02 +0000

    full site reset and rebuild

--- app/pricing/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Pricing() {
-  return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold">Pricing</h1>
-    </main>
-  )
-}
